b6ff20c205faeea05eb7ea0f60d9aff297df240a
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-08-12 09:42:06 -0500

    updated mongoose connection

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-//mongoose.connect("mongodb://localhost/budget", {
- // useNewUrlParser: true,
- // useFindAndModify: false
-//});
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+//mongoose.connect(
+  //process.env.MONGODB_URI || "mongodb://localhost/budget",
+  //{ useNewUrlParser: true, useUnifiedTopology: true }
+//);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
